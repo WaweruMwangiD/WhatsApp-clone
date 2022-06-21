aeb1e9e518a0612f5704ca22549976464179256a
WaweruMwangiD <wawerumwangi21@gmail.com>
2022-06-21 06:54:38 +0300

    work on sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,8 @@
-import { Avatar } from '@material-ui/core';
-import { Chat, DonutLarge, MoreVert } from '@material-ui/icons';
+import { Avatar, IconButton } from '@material-ui/core';
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React from 'react'
 import './Sidebar.css'
+import SidebarChats from './SidebarChats';
 
 const Sidebar = () => {
   return (
@@ -9,17 +10,32 @@ const Sidebar = () => {
       <div className="sidebar_header">
         <Avatar />
         <div className='sidebar_headerRight'>
-          <DonutLarge />
-          <Chat />
-          <MoreVert />
-        
-        
+         
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+          
         </div>
 
       </div>
       <div className="sidebar_search">
+        <div className='sidebar_searchContainer'>
+          <SearchOutlined />
+          <input placeholder='Search or start new chat' type="text" />
+
+        </div>
+       
       </div>
       <div className="sidebar_chats">
+        <SidebarChats />
+
       </div>
     
     
---
+++ src/SidebarChats.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SidebarChats.css'
+
+const SidebarChats = () => {
+  return (
+    <div>SidebarChats</div>
+  )
+}
+
+export default SidebarChats
